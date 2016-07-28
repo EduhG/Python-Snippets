2147b43e1e4e05fd3e3aca28e9546124bc3fd1a8
eduhg <edwinnyangena@gmail.com>
2016-07-28 22:28:49 +0300

    Formated printed data of found record

--- ReadFiles/searchJsonFiles.py
+++ ReadFiles/searchJsonFiles.py
@@ -6,11 +6,13 @@ with open("data.json") as json_file:
 userId = int(input("Enter your id: ")) - 1
 
 if int(json_data[userId]["id"]) - 1 == userId:
-    print(json_data[userId]["id"])
-    print(json_data[userId]["first_name"])
-    print(json_data[userId]["last_name"])
-    print(json_data[userId]["email"])
-    print(json_data[userId]["gender"])
+    print(
+        "User ID:\t" + str(json_data[userId]["id"]) + "\n" +
+        "First Name:\t" + json_data[userId]["first_name"] + "\n" +
+        "Last Name:\t" + json_data[userId]["last_name"] + "\n" +
+        "Email:\t\t" + json_data[userId]["email"] + "\n" +
+        "Gender:\t\t" + json_data[userId]["gender"])
+
 else:
     print("User not found")
 
@@ -19,6 +21,6 @@ for i in range(len(json_data)):
     if json_data[i]["gender"] == "Male":
         count += 1
 
-print("\nData Summary ..........................................\n")
-print("Number of Females: \t" + str(len(json_data) - count))
-print("Number of Males: \t" + str(count))
+print("\nData Summary ..........................................")
+print("\tNumber of Females: \t" + str(len(json_data) - count))
+print("\tNumber of Males: \t" + str(count))
